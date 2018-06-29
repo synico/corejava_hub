071a1c87bfbc87711fa748fb2c3285e5cd75e188
nick@p50 <synico@gmail.com>
2018-06-29 18:02:50 +0800

    update demo code for learning lambda expression
    Signed-off-by: nick@p50 <synico@gmail.com>

--- java_concurrency/src/main/java/javac/stream/LambdaDemo.java
+++ java_concurrency/src/main/java/javac/stream/LambdaDemo.java
@@ -2,6 +2,9 @@ package javac.stream;
 
 import java.util.ArrayList;
 import java.util.List;
+import java.util.function.Function;
+import java.util.function.Predicate;
+import java.util.function.Consumer;
 import java.util.stream.Collectors;
 
 
@@ -13,13 +16,35 @@ public class LambdaDemo {
         Order order2 = new Order("order2", 2, 2.2);
         Order order3 = new Order("order3", 3, 3.3);
         Order order4 = new Order("order4", 4, 4.4);
-//        orders.add(order1);
-//        orders.add(order2);
-//        orders.add(order3);
-//        orders.add(order4);
+        orders.add(order1);
+        orders.add(order2);
+        orders.add(order3);
+        orders.add(order4);
 
-        orders = orders.stream().filter(order -> !(order.orderId == 1)).collect(Collectors.toList());
-        orders.stream().map(order -> order.getOrderId()).forEach(System.out :: println);
+        Predicate<Order> orderIdIsOne = o -> o.getOrderId() == 1;
+        Predicate<Order> orderIdIsNotTwo = o -> o.getOrderId() != 2;
+
+        Function<Order, String> mapOrderId2Str = o -> o.getOrderId().toString();
+
+        Consumer<Order> peekItemBeforeFilter = o -> System.out.println("before stream is processed: " + o.getOrderId());
+        Consumer<String> peekItemAfterFiler = e -> System.out.println("after stream is filtered: " + e);
+        Consumer<String> printElement = System.out::println;
+
+        orders.stream()
+                .filter(orderIdIsOne)
+                .collect(Collectors.toList());
+
+        orders.stream()
+                .peek(peekItemBeforeFilter)
+                .filter(orderIdIsNotTwo)
+                .map(mapOrderId2Str)
+                .peek(peekItemAfterFiler)
+                .forEach(printElement);
+
+        int value = 4;
+        Runnable run = () -> System.out.println("runnable:" + value);
+
+        System.out.println("End...." + printElement);
     }
 
     public static void main(String args[]) {
