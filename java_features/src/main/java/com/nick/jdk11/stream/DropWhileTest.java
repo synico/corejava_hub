1205b907502000c27bcb6db95ae56479139e7089
Nick Liu <xuewei.liu@ge.com>
2019-10-15 17:08:16 +0800

    demo about lambda

--- java_features/src/main/java/com/nick/jdk11/stream/DropWhileTest.java
+++ java_features/src/main/java/com/nick/jdk11/stream/DropWhileTest.java
@@ -6,14 +6,14 @@ import java.util.stream.Stream;
 public class DropWhileTest {
 
     public static void main(String[] args) {
-        long count = Stream.of(1, 2, 3, 4, 5).dropWhile(i -> i % 2 != 0).count();
-
-        var list = new ArrayList<String>();
-        var stream = list.stream();
-
-        var test = 1;
-
-        System.out.println(test);
+//        long count = Stream.of(1, 2, 3, 4, 5).dropWhile(i -> i % 2 != 0).count();
+//
+//        var list = new ArrayList<String>();
+//        var stream = list.stream();
+//
+//        var test = "1";
+//
+//        System.out.println(test);
     }
 
 }
--- java_features/src/main/java/com/nick/jdk8/lambdas/Lambda1.java
+++ java_features/src/main/java/com/nick/jdk8/lambdas/Lambda1.java
@@ -1,4 +1,54 @@
 package com.nick.jdk8.lambdas;
 
+import java.math.BigDecimal;
+import java.util.function.BinaryOperator;
+import java.util.function.Consumer;
+import java.util.function.Function;
+import java.util.function.Supplier;
+import java.util.function.UnaryOperator;
+import lombok.Data;
+import java.util.function.Predicate;
+
 public class Lambda1 {
+
+  public static void main(String[] args) {
+    Predicate<Integer> predicate = x -> x > 100;
+    Student student = new Student("9Dragon", 23, 175);
+    System.out.println("Is 9Dragon higher than 100? : " + predicate.test(student.getHeight()));
+
+    Consumer<String> consumer = System.out::println;
+    consumer.accept("Yes, I am");
+
+    Function<Student, String> function = Student::getName;
+    String name = function.apply(student);
+    System.out.println(name);
+
+    Supplier<Integer> supplier = () -> Integer.valueOf(BigDecimal.TEN.toString());
+    System.out.println(supplier.get());
+
+    UnaryOperator<Boolean> unaryOperator = uglily -> !uglily;
+    Boolean apply2 = unaryOperator.apply(true);
+    System.out.println(apply2);
+
+    BinaryOperator<Integer> operator = (x, y) -> x * y;
+    Integer result = operator.apply(2, 3);
+    System.out.println(result);
+  }
+}
+
+@Data
+class Student {
+
+  private String name;
+
+  private Integer age;
+
+  private Integer height;
+
+  public Student(String name, Integer age, Integer height) {
+    this.name = name;
+    this.age = age;
+    this.height = height;
+  }
+
 }
